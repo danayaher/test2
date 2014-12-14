818cc9696655a80a92b200b4d89df516d003380a
Daniel Ayala Hernández <danayaher@alum.us.es>
2014-12-14 18:55:11 +0100

    Test
    testalo

--- test.php
+++ test.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Saludo
+ * 
+ * Este archivo muestra un saludo.
+ *
+ * PHP version 5
+ *
+ * @category None
+ * @package  None
+ * @author   Daniel Ayala Hernández <danayaher@alum.us.es>
+ * @license  http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @link     github/blablabla
+ */
+
 $number = 2;
-echo $number;
+$false = FALSE;
+echo "hola";
 ?>
